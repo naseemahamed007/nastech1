8f39c48331a82eeff09171119abc73560d5c64f5
naseemahamed007 <naseemishere0@gmail.com>
2026-03-09 00:11:47 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white/10 backdrop-blur-lg text-white text-center p-6 mt-10">
+      <p>&copy; {new Date().getFullYear()} Nastech Innovation. All rights reserved.</p>
+    </footer>
+  )
+}
