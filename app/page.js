22f50087aee4616196606e1a463b49a7e04f9e0e
naseemahamed007 <naseemishere0@gmail.com>
2026-03-09 00:09:02 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import ContactForm from "../components/ContactForm"
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Services />
+      <ContactForm />
+    </main>
+  )
+}
