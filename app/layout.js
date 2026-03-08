ffed11dc223f276dd15cb1c200befd6dce4155e0
naseemahamed007 <naseemishere0@gmail.com>
2026-03-09 00:27:13 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
 import "../styles/globals.css"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
-        <Navbar />
+      <body>
         {children}
-        <Footer />
       </body>
     </html>
   )
